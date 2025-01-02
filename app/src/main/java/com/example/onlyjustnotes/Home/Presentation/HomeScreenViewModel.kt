beb13a375a3ffdf1e0abd3d646443411dad31e2b
Roman <roman.aaa.06@inbox.ru>
2025-01-02 17:18:12 +0500

    added dagger module

---
+++ app/src/main/java/com/example/onlyjustnotes/Home/Presentation/HomeScreenViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.onlyjustnotes.Home.Presentation
+
+class HomeScreenViewModel {
+}
--- app/src/main/java/com/example/onlyjustnotes/Home/di/HomeScreenModule.kt
+++ app/src/main/java/com/example/onlyjustnotes/Home/di/HomeScreenModule.kt
@@ -1,4 +1,24 @@
 package com.example.onlyjustnotes.Home.di
 
+import com.example.onlyjustnotes.Home.Domain.DeleteNoteUseCase
+import com.example.onlyjustnotes.Home.Domain.GetAllNotesUseCase
+import com.example.onlyjustnotes.Home.Domain.Impl.DeleteNoteUseCaseImpl
+import com.example.onlyjustnotes.Home.Domain.Impl.GetAllNotesUseCaseImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
 abstract class HomeScreenModule {
+    @Binds
+    abstract fun bindGetAllNotesUseCase(
+        getAllNotesUseCaseImpl: GetAllNotesUseCaseImpl
+    ): GetAllNotesUseCase
+
+    @Binds
+    abstract fun bindDeleteNoteUseCase(
+        deleteNoteUseCaseImpl: DeleteNoteUseCaseImpl
+    ):DeleteNoteUseCase
 }
